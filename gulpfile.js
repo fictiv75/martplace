0937bb36f04929ed1e8d3c055e46962d7be6b443
Pavel <fictiv75@gmail.com>
2020-10-02 15:19:01 +0300

    added page pricing

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ let gulp = require("gulp"),
 gulp.task("sass", function () {
   return gulp
     .src("app/scss/**/*.scss")
-    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(sass({ outputStyle: "compressed" }))
     .pipe(rename({ suffix: ".min" }))
     .pipe(
       autoprefixer({
